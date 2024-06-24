b99e8268660265a518c9f582d604f828112b1879
tharun <tharun@gmail.com>
2024-06-23 23:02:46 -0500

    Redundant API calls are happening #9

--- src/components/NotFound/Header.jsx
+++ src/components/NotFound/Header.jsx
@@ -81,7 +81,7 @@ const handleCloseModal=()=>{
                 </Toolbar>
             </AppBar>
         </Box>
-        {<ManageProfile open={openModal} handleCloseModal={handleCloseModal}/>}
+        {openModal && <ManageProfile open={openModal} handleCloseModal={handleCloseModal}/>}
         </>
     );
 }
