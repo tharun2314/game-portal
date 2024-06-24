39e3dd0c451ec252ac05cf3e474b341b081aa7fb
tharun <tharunpadmanabhasomashekar@my.unt.edu>
2024-06-23 23:45:45 -0500

    Scroll bar at bottom of page #10

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,47 +1,42 @@
-import React, { useState } from 'react'
-import Header from '../components/NotFound/Header'
-import BasicCard from '../components/NotFound/BasicCard'
-import Snakegame from '../components/NotFound/Snakegame'
-import Sidebar from '../components/NotFound/SideBar'
+import React, { useState } from 'react';
+import Header from '../components/NotFound/Header';
+import BasicCard from '../components/NotFound/BasicCard';
+import Sidebar from '../components/NotFound/SideBar';
 import Grid from '@mui/material/Grid';
-import snake from "../components/images/snake-image.png"
-import shooter from "../components/images/shooter.png"
-import { useNavigate } from "react-router-dom"
-import Gamelevel from '../components/Gamelevel'
+import snake from "../components/images/snake-image.png";
+import shooter from "../components/images/shooter.png";
+import { useNavigate } from "react-router-dom";
+import Gamelevel from '../components/Gamelevel';
+import { Box } from '@mui/material';
 
+export default function HomePage() {
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [path, setPath] = useState("");
 
-export default function HomePage()
-{
-    const navigate = useNavigate()
-    const [open,setOpen]=useState(false);
-    const [path,setPath]=useState("");
+  const clickPlay = (event) => {
+    console.log("I am heree....", event.target);
+    setPath(event.target.id);
+    setOpen(true);
+  };
 
-    const clickPlay=(event)=>{
-        console.log("I am heree....",event.target)
-        setPath(event.target.id)
-        setOpen(true);  
-         
-    }
-    const handleCloseModal=()=>{
-        setOpen(false);
-    }
-  
-    return(
-        <div>
-             <Header/>
-        <Sidebar/>
-        <Grid container style={{marginLeft:300,marginTop:20}}spacing={2}>
-            <Grid item>
-            <BasicCard name={"Snake game"} id="/snake-game"  onClick={clickPlay} component={snake} />
-            </Grid>
-            <Grid item>
-            <BasicCard name={"Shooter Game"} id="/shooter-game" onClick={clickPlay} component={shooter} />
-            </Grid>
-           {open && <Gamelevel open={open} path={path} handleCloseModal={handleCloseModal}/>}
-            
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
 
+  return (
+    <Box sx={{ overflowX: 'hidden', padding: 0, margin: 0 }}>
+      <Header />
+      <Sidebar />
+      <Grid container spacing={2} sx={{ marginLeft:30, padding: 2 }}>
+        <Grid item xs={12} sm={6} md={4}>
+          <BasicCard name="Snake game" id="/snake-game" onClick={clickPlay} component={snake} />
         </Grid>
-       </div>
-
-    )
+        <Grid item xs={12} sm={6} md={4}>
+          <BasicCard name="Shooter Game" id="/shooter-game" onClick={clickPlay} component={shooter} />
+        </Grid>
+        {open && <Gamelevel open={open} path={path} handleCloseModal={handleCloseModal} />}
+      </Grid>
+    </Box>
+  );
 }
--- src/components/NotFound/BasicCard.jsx
+++ src/components/NotFound/BasicCard.jsx
@@ -6,38 +6,27 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
-import snake from "../../images/snake.jpg"
-
-
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
 
 export default function BasicCard(props) {
   return (
     <Card sx={{ minWidth: 275 }} id={props.id}>
       <CardContent>
-        <Typography variant="headline" sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
-         {props.name}
+        <Typography variant="h5" sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
+          {props.name}
         </Typography>
-        <Typography component="div">
-        <img src={props.component} style={{width:300,height:200}}/>
-        </Typography>
-  
+        <CardMedia
+          component="img"
+          height="200"
+          image={props.component}
+          alt={props.name}
+        />
         <Typography component="p">
-        {props.description}
+          {props.description}
         </Typography>
       </CardContent>
       <CardActions>
-      <Button variant="contained" onClick={props.onClick} id={props.id}>Play</Button>
-        </CardActions>
-   
+        <Button variant="contained" onClick={props.onClick} id={props.id}>Play</Button>
+      </CardActions>
     </Card>
   );
 }
